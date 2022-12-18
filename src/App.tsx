ecef9c70fa8e5215ac2450405360974326c04a9f
psllllll <1448071611@qq.com>
2022-12-18 11:03:31 +0800

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './style/App.css'
 import Header from './component/Header'
 import Swiper from './component/Swiper'
+import Footer from './component/PageFooter'
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Swiper></Swiper>
+      <Footer></Footer>
     </div>
   )
 }
